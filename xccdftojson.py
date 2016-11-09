fcf5f23399e1f434dd55b4a782d1e68dc3d913ce
myersmk <mkmyers@ihaveroot.com>
2016-11-09 11:39:33 -0500

    initial commit

---
+++ xccdftojson.py
@@ -0,0 +1,38 @@
+#!/bin/python
+
+import os
+import pprint
+import xml.dom.minidom
+import json
+from xml.dom.minidom import Node
+
+
+stig_doc = xml.dom.minidom.parse("U_Red_Hat_Enterprise_Linux_7_STIG_V1R0-2_Manual-xccdf.xml")
+
+mapping = {}
+
+for node in stig_doc.getElementsByTagName("Group"):    
+            
+    vuln_id = node.getAttribute("id")    
+    group_id = node.getElementsByTagName("title")[0].firstChild.data
+    description = node.getElementsByTagName("title")[1].firstChild.data
+    rules = node.getElementsByTagName("Rule")    
+                     
+    for node2 in rules:        
+        severity = node2.getAttribute("severity")
+              
+        sub_maps = {
+            'GROUP' : group_id ,
+            'SEVERITY' : severity,
+            'DESCRIPTION' : description 
+        }
+       
+    mapping[vuln_id] = sub_maps    
+    
+with open("stig.json","w") as f:
+    json.dump(mapping,f)       
+       
+#print(mapping)
+ 
+ 
+

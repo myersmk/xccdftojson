3afb2cce5fbd62b44bd7b884ad252c8b737cbbc0
myersmk <mkmyers@ihaveroot.com>
2016-11-09 17:48:39 -0800

    removed print and import for Node

--- xccdftojson.py
+++ xccdftojson.py
@@ -4,35 +4,36 @@ import os
 import pprint
 import xml.dom.minidom
 import json
-from xml.dom.minidom import Node
+import sys
 
 
-stig_doc = xml.dom.minidom.parse("U_Red_Hat_Enterprise_Linux_7_STIG_V1R0-2_Manual-xccdf.xml")
+#stig_doc = xml.dom.minidom.parse("U_Red_Hat_Enterprise_Linux_7_STIG_V1R0-2_Manual-xccdf.xml")
+
+stig_doc = xml.dom.minidom.parse(sys.argv[1])
 
 mapping = {}
 
-for node in stig_doc.getElementsByTagName("Group"):    
-            
-    vuln_id = node.getAttribute("id")    
+for node in stig_doc.getElementsByTagName("Group"):
+
+    vuln_id = node.getAttribute("id")
     group_id = node.getElementsByTagName("title")[0].firstChild.data
     description = node.getElementsByTagName("title")[1].firstChild.data
-    rules = node.getElementsByTagName("Rule")    
-                     
-    for node2 in rules:        
+    rules = node.getElementsByTagName("Rule")
+
+
+    for node2 in rules:
         severity = node2.getAttribute("severity")
-              
+
         sub_maps = {
             'GROUP' : group_id ,
             'SEVERITY' : severity,
-            'DESCRIPTION' : description 
+            'DESCRIPTION' : description,
+            #'DETAILS' : details
         }
-       
-    mapping[vuln_id] = sub_maps    
-    
-with open("stig.json","w") as f:
-    json.dump(mapping,f)       
-       
-#print(mapping)
- 
- 
 
+    mapping[vuln_id] = sub_maps
+
+with open("stig.json","w") as f:
+    json.dump(mapping,f)
+
+#print(mapping)

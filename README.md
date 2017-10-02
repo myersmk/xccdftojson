f9307cd0f81f69c19cf97793aa99ae385434924b
Myers, Matthew (US) <matthew.myers2@baesystems.com>
2017-10-02 15:15:15 -0700

    added all elements and attributes from xml

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
-## V1R0-1 Draft Benchmark
-1. The script takes a single argument (benchmark xml file). 
+## V1R2 Draft Benchmark
+1. The script takes a single argument (benchmark xml file). This file can be retrieved from [IASE | DISA]: https://iase.disa.mil/stigs/os/unix-linux/Pages/index.aspx
 ```bash
-./xccdftojson U_Red_Hat_Enterprise_Linux_7_STIG_V1R0-1_Manual-xccdf.xml
+./xccdftojson U_Red_Hat_Enterprise_Linux_7_STIG_V1R2_Manual-xccdf.xml
 ``` 
 
-2. Prints to stdout and writes a copy within the current directory
+2. Writes a copy within the current directory
--- xccdftojson.py
+++ xccdftojson.py
@@ -1,7 +1,6 @@
 #!/bin/python
 
 import os
-import pprint
 import xml.dom.minidom
 import json
 import sys
@@ -14,26 +13,68 @@ stig_doc = xml.dom.minidom.parse(sys.argv[1])
 mapping = {}
 
 for node in stig_doc.getElementsByTagName("Group"):
-
+    title1 = node.getAttribute("title")
     vuln_id = node.getAttribute("id")
     group_id = node.getElementsByTagName("title")[0].firstChild.data
-    description = node.getElementsByTagName("title")[1].firstChild.data
-    rules = node.getElementsByTagName("Rule")
+    description1 = node.getElementsByTagName("description")[1].firstChild.data
 
-
-    for node2 in rules:
+    for node2 in node.getElementsByTagName("Rule"):
+        rule_id = node2.getAttribute("id")
         severity = node2.getAttribute("severity")
+        weight = node2.getAttribute("weight")
+        version = node2.getElementsByTagName("version")[0].firstChild.data
+        title2 = node2.getElementsByTagName("title")[0].firstChild.data
+        description2 = node2.getElementsByTagName("description")[0].firstChild.data
+        # ident is a URL http://iase.disa.mil/cci and returns a 404
+        #ident = node2.getAttribute("system")
+        cci = node2.getElementsByTagName("ident")[0].firstChild.data
+        fixtext = node2.getElementsByTagName("fixtext")[0].firstChild.data
 
-        sub_maps = {
+        if severity == "high":
+          cat = "CAT-I"
+        elif severity == "medium":
+          cat = "CAT-II"
+        elif severity == "low":
+          cat = "CAT-III"
+        else:
+          cat = "UNKNOWN"
+
+    for fixes in node2.getElementsByTagName("fixtext"):
+      fixref = fixes.getAttribute("fixref")
+          
+    for fix_ids in node2.getElementsByTagName("fix"):
+      fix_id = fix_ids.getAttribute("id")
+  
+    for checks in node2.getElementsByTagName("check"):
+      check = checks.getAttribute("system")
+      check_content = checks.getElementsByTagName("check-content")[0].firstChild.data
+
+    sub_maps = [
+      {
             'GROUP' : group_id ,
+            'TITLE': title1,
+            'DESCRIPTION': description1,
+            'ID': rule_id,
             'SEVERITY' : severity,
-            'DESCRIPTION' : description,
-            #'DETAILS' : details
-        }
+            'CAT': cat,
+            'WEIGHT': weight,
+            'VERSION': version,
+            'TITLE': title2,
+            'DESCRIPTION': description2,
+            #'IDENT': ident,
+            'CCI': cci,
+            'FIXREF': fixref,
+            'FIXTEXT': fixtext,
+            'FIX_ID': fix_id,
+            'FIX_CHECK': check,
+            'CHECK_CONTENT': check_content
+      },
+        ]
 
     mapping[vuln_id] = sub_maps
 
-with open("stig.json","w") as f:
+
+with open(sys.argv[1].replace('xml', 'json'),"w") as f:
     json.dump(mapping,f)
 
 print(mapping)
